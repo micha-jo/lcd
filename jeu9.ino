86612ff02ddadc84baa0b0635aa76667fca7386d
joachim Renevier <renevierjoachim@gmail.com>
2023-04-16 11:26:41 +0200

    mes mechant fait

--- jeu9.ino
+++ jeu9.ino
@@ -6,22 +6,36 @@
 Parametre mesParametres; 
 Position mesPositions; 
 Hero monHero(&mesPositions); 
-
+Mechant monMechant(&mesPositions); 
+SecondaryScreen monEcranSecondaire(&monHero); 
 void setup()
 {
   mesParametres.initialisation(hero,mechant,obstacle,key,door,life,trap,arrow); 
-  mesPositions.generatePosition(1,5,10,1,1,1,1,1,1,1,1,1); 
-  mesPositions.setSize(25,10);
+  mesPositions.generatePosition(1,10,2,1,1,1,1,1,1,1,1,1); 
+  mesPositions.setSize(25,3);
   mesPositions.typeZelda(); 
+  monMechant.createMechant(); 
+
 }
 void loop()
 {
-  mesPositions.manualShift();
+  mesPositions.manualShiftBorder();
   
   monHero.move();
-  Serial.print(mesPositions.ary[0][0][0]); 
-  Serial.print("-"); 
-  Serial.println(mesPositions.ary[0][0][1]); 
+  monHero.pullWithRequest(1); 
+  monHero.interaction(); 
+
+  monEcranSecondaire.displaySecondaryScreen(2);
+  
+  
+  //monHero.jumpWithRequest(1);
+  // Serial.print(mesPositions.ary[6][0][0]); 
+  // Serial.print("-"); 
+  // Serial.print(mesPositions.ary[6][0][1]); 
+  // Serial.print(" "); 
+  // Serial.print(mesPositions.ary[7][0][0]); 
+  // Serial.print("-"); 
+  // Serial.println(mesPositions.ary[7][0][1]); 
 } 
 
 
